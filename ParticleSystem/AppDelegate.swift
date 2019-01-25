acabfb629cc7069f80e3e9e3131aa1b34136a4de
Brenna Olson <36449761+brennadev@users.noreply.github.com>
2019-01-25 07:43:06 -0600

    Initial Commit

---
+++ ParticleSystem/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  ParticleSystem
+//
+//  Created by Brenna Olson on 1/25/19.
+//  Copyright © 2019 Brenna Olson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
