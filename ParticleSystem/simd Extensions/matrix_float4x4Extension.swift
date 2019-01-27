332732143410f20eabbb71fb53dd127c569c03df
Brenna Olson <36449761+brennadev@users.noreply.github.com>
2019-01-27 14:29:40 -0600

    identity matrix definition (convenience)

---
+++ ParticleSystem/simd Extensions/matrix_float4x4Extension.swift
@@ -0,0 +1,17 @@
+//
+//  matrix_float4x4Extension.swift
+//  ParticleSystem
+//
+//  Created by Brenna Olson on 1/27/19.
+//  Copyright © 2019 Brenna Olson. All rights reserved.
+//
+
+import simd
+
+// aka matrix_float4x4
+extension matrix_float4x4 {
+    /// 4x4 identity matrix
+    static var identity: matrix_float4x4 {
+        return matrix_float4x4(float4(1, 0, 0, 0), float4(0, 1, 0, 0), float4(0, 0, 1, 0), float4(0, 0, 0,1))
+    }
+}
