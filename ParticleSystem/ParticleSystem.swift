27232826771eb669ed4605ecc1356fdd6b4d8c81
Brenna Olson <36449761+brennadev@users.noreply.github.com>
2019-02-03 15:13:13 -0600

    determining number of particles to generate

--- ParticleSystem/ParticleSystem.swift
+++ ParticleSystem/ParticleSystem.swift
@@ -10,19 +10,17 @@ import simd
 
 /// Singleton to manage all the particles in use in the particle system. Will manage empty holes in the array from dead particles and fill them as needed when new particles are created.
 struct ParticleSystem {
-    /// Only here to prevent instances of the struct from being created
-    private init() {
-    }
-    
+ 
+    // MARK: - Particle Storage
     /// All the particles in the system. May contain dead particles, which can be checked by looking at an element's `isAlive` property.
     /// - note: Particles are removed by setting an individual element's `isAlive` property to `false`.
-    private(set) static var allParticles = [Particle]()
+    private(set) var allParticles = [Particle]()
     
     /// All available indices due to dead particles
-    private static var emptyIndices = [Int]()
+    private var emptyIndices = [Int]()
     
     /// Add a new particle into the system
-    static func addParticle(newParticle: Particle) {
+    mutating func addParticle(newParticle: Particle) {
         // when there's an available space due to a dead particle, use it
         if let last = emptyIndices.last {
             allParticles[last] = newParticle
@@ -32,5 +30,26 @@ struct ParticleSystem {
         } else {
             allParticles.append(newParticle)
         }
+    }
+    
+    
+    // MARK: - Initial Generation
+    /// Particles per second to generate
+    private let particleGenerationRate = 10
+    
+    func numberOfParticlesToGenerate(in dt: Float) -> Int {
+        
+        
+        let numberOfParticles = Float(particleGenerationRate) * dt
+        let numberOfParticlesRoundedDown = Int(numberOfParticles.rounded(.down))
+        // not sure where this is supposed to be used
+        let numberOfParticlesFraction = numberOfParticles - Float(numberOfParticlesRoundedDown)
+        
+        if Float.random(in: 0...100) < dt * Float(particleGenerationRate) {
+            return numberOfParticlesRoundedDown + 1
+        } else {
+            return numberOfParticlesRoundedDown
+        }
+        
     }
 }
